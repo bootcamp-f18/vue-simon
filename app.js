f33de0b539184adadcf139448a36431155c76727
Janine Hempy <jhempy@gmail.com>
2018-10-16 14:18:33 -0400

    Adds timer component

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
+Vue.component('timer', {
 
+    template: `<div id="status">
+                    <span v-html="message"></span>{{ remaining }}
+                </div>`,
 
+    data() {
+        return {
+            message: 'Click the <strong>Start Game</strong> button to begin!',
+            remaining: ''
+        }
+    },
 
+    created() {
 
+        let self = this;
+
+        self.$parent.$on('stateChange', function(event) {
+
+            switch (event) {
+
+                case 'capturing':
+                    // do stuff
+                    // break;
+
+                case 'playing':
+                    // do stuff
+                    // break;
+
+                default:
+                    console.log("Timer: state changed to [" + event + "]");
+
+            }
+
+        });
+
+    }
+
+});
 
 var app = new Vue({
 
     el: "#app",
 
     data: {
-        message: 'Click the "Start Game" button to begin.',
         currentButton: '',
         sequence: ['blue', 'red', 'red', 'yellow', 'green', 'yellow'],
         playingId: 0
@@ -18,7 +52,7 @@ var app = new Vue({
 
         tap: function (color) {
 
-            var self = this;
+            let self = this;
 
             self.currentButton = color;
             setTimeout(function() {
@@ -28,10 +62,11 @@ var app = new Vue({
 
         playSequence: function () {
 
-            var self = this;
-            self.message = "Play attention...here we go!";
+            let self = this;
+            // self.message = "Play attention...here we go!";
+            self.$emit('stateChange', 'playing');
 
-            var intervalId = setInterval(function () {
+            let intervalId = setInterval(function () {
 
                 if (self.playingId < self.sequence.length) {
                     self.tap(self.sequence[self.playingId]);
@@ -40,7 +75,8 @@ var app = new Vue({
                 else {
                     clearInterval(intervalId);
                     self.playingId = 0;
-                    self.message = "Your turn!";
+                    // self.message = "Your turn!";
+                    self.$emit('stateChange', 'capturing');
                 }
 
             }, 600);
